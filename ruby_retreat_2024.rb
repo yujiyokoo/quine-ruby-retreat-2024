ef8cea4e0b0ab8c75539c212c70262b406bf4265
Yuji Yokoo <yujiyokoo@gmail.com>
2024-10-19 20:16:52 +1100

    Apply formatting to Ruby source

--- ruby_retreat_2024.rb
+++ ruby_retreat_2024.rb
@@ -1,351 +1,52 @@
-eval$s=%w(
-  s=%(eval$s=%w(#{$s})*"");
-  f = -> n { s.slice!(0, n) };
-  pf = -> n { print(s.slice!(0, n)) };
-  sp = -> n { print(32.chr * n) };
-  row = ->(&b) { pf[4]; sp[4]; b.call; sp[4]; pf[4]; puts; pf[4]; sp[4]; b.call; sp[4]; pf[4]; puts;};
 
-  puts;
+eval$s=%w(s=%(eval$s=%w(#{$s})*"");f=->n{s.slice!(0,n)};pf=->n{print(s.slice!(0,n))};sp=->n{print(32.chr*n)};row=->(&b){pf[4];sp[4];b.call;s
+p[4]                                                                                                                                    ;pf[
+4];p                                                                                                                                    uts;
+pf[4    ];sp[4];b.call                          ;sp[                                                                                    4];p
+f[4]    ;puts;};puts;p                          f[14                                                                                    0];p
+uts;    row.        call                        {sp[                                                                                    124]
+;};r    ow.c        all{                        pf[1                                                                                    4];s
+p[26    ];pf[4];sp[80]      ;};r        ow.c    all{pf[4];sp      [8];          pf[4                                                    ];sp
+[24]    ;pf[4];sp[80];      };ro        w.ca    ll{pf[14];sp      [6];          pf[4                                                    ];sp
+[8];    pf[4];sp            [4];        pf[1    2];s      p[6];p    f[4]        ;sp[                                                    10];
+pf[4    ];sp[48]            ;};r        ow.c    all{      pf[8];    sp[1        2];p                                                    f[4]
+;sp[    8];p    f[4]        ;sp[    4];pf[4]    ;sp[        6];p    f[6]    ;sp[4]                                                      ;pf[
+4];s    p[8]    ;pf[        4];s    p[48];};    row.        call    {pf[    4];sp[                                                      4];p
+f[4]    ;sp[      8];pf[      4];sp[4]  ;pf[    8];sp[4];pf[4]          ;sp[8]                                                          ;pf[
+4];s    p[4]      ;pf[4]      ;sp[4];p  f[6]    ;sp[50];};row.          call{p                                                          f[4]
+;sp[                                                                6];pf[                                                              6];s
+p[6]                                                                ;pf[8]                                                              ;sp[
+2];p                                                          f[4];sp[                                                                  4];p
+f[14                                                          ];sp[10]                                                                  ;pf[
+6];s        p[54];};row.ca                              ll{s                                                                p[60        ];pf
+[6];        sp[58];};row.c                              all{                                                                sp[5        4];p
+f[8]        ;sp[        62];      };row.call{s      p[4];pf[14];    sp[3    0];p      f[4];sp[64];        pf[4];sp[4    ];};row.call    {sp[
+4];p        f[4]        ;sp[      8];pf[4];sp[      6];pf[12];sp    [6];    pf[1      2];sp[4];pf[        4];sp[4];p    f[4];sp[6];p    f[12
+];sp        [8];pf[10];sp[      4];p        f[12        ];};        row.call{s      p[4]        ;pf[    14];    sp[6        ];pf        [4];
+sp[8        ];pf[4];sp[8];      pf[4        ];sp        [8];        pf[10];sp[      6];p        f[4]    ;sp[    8];p        f[4]        ;sp[
+4];p        f[4];sp[            4];pf[4];sp[8];p        f[4]        ;sp[4]          ;};row.call{sp[4    ];pf    [8];        sp[1        2];p
+f[16        ];sp[8];            pf[4];sp[8];pf[6        ];sp        [10];p          f[16];sp[4];pf[4    ];sp    [4];        pf[4        ];sp
+[8];        pf[4    ];sp        [4];                    };ro        w.ca            ll{s                p[4]    ;pf[4]      ;sp[        4];p
+f[4]        ;sp[    8];p        f[4]                    ;sp[        20];            pf[4                ];sp    [8];pf      [4];        sp[1
+2];p        f[4]      ;sp[16      ];pf[4];sp              [4];pf    [6];              sp[6];pf[4          ];sp[4    ];};      row.ca    ll{s
+p[4]        ;pf[      4];sp[      6];pf[6];s              p[6];p    f[10              ];sp[14];p          f[6];s    p[4]      ;pf[4]    ;sp[
+14];                                                                                                                                    pf[1
+0];s                                                                                                                                    p[10
+];pf                            [6];sp[4            ];pf[4];            sp[6];pf                  [6];};                                row.
+call                            {sp[124]            ;};row.c            all{sp[2                  4];pf[                                8];s
+p[12                        ];pf[8    ];sp[1    2];pf[    8];sp[    18];pf    [6];sp            [28];};r                                ow.c
+all{                        sp[20]    ;pf[6]    ;sp[4]    ;pf[6]    ;sp[4]    ;pf[6]            ;sp[4];p                                f[6]
+;sp[                        4];p        f[6]    ;sp[        4];p    f[6]        ;sp[        12];pf  [8];                                sp[2
+8];}                        ;row        .cal    l{sp        [20]    ;pf[        4];s        p[8];p  f[4]                                ;sp[
+4];p                                f[4];s      p[8]        ;pf[            4];sp[        4];p      f[4]                                ;sp[
+8];p                                f[4];s      p[8]        ;pf[            6];sp[        2];p      f[4]                                ;sp[
+28];                            };row.          call        {sp[        28];pf          [6];sp[6];pf[4];sp[8                            ];pf
+[4];                            sp[12]          ;pf[        6];s        p[8];p          f[4];sp[6];pf[4];sp[                            28];
+};ro                        w.ca                ll{s        p[24    ];pf                            [6];                                sp[1
+0];p                        f[4]                ;sp[        8];p    f[4]                            ;sp[                                8];p
+f[6]                        ;sp[10];pf[20];s        p[24];};        row.call{sp[20];                pf[4                                ];sp
+[16]                        ;pf[4];sp[8];pf[        4];sp[4]        ;pf[4];sp[28];pf                [4];                                sp[2
+8];}                                                                                                                                    ;row
+.cal                                                                                                                                    l{sp
+[20];pf[16];sp[8];pf[8];sp[8];pf[16];sp[16];pf[4];sp[28];};row.call{sp[124];};pf[140];puts;pf[140];puts;###padding######padding#########)*""
 
-  pf[140];
-  puts;
-
-  row.call {
-    sp[124];
-  };
-
-  row.call {
-    pf[14];
-    sp[26];
-    pf[4];
-    sp[80];
-  };
-
-  row.call {
-    pf[4];
-    sp[8];
-    pf[4];
-    sp[24];
-    pf[4];
-    sp[80];
-  };
-
-  row.call {
-    pf[14];
-    sp[6];
-    pf[4];
-    sp[8];
-    pf[4];
-    sp[4];
-    pf[12];
-    sp[6];
-    pf[4];
-    sp[10];
-    pf[4];
-    sp[48];
-  };
-
-  row.call {
-    pf[8];
-    sp[12];
-    pf[4];
-    sp[8];
-    pf[4];
-    sp[4];
-    pf[4];
-    sp[6];
-    pf[6];
-    sp[4];
-    pf[4];
-    sp[8];
-    pf[4];
-    sp[48];
-  };
-
-  row.call {
-    pf[4];
-    sp[4];
-    pf[4];
-    sp[8];
-    pf[4];
-    sp[4];
-    pf[8];
-    sp[4];
-    pf[4];
-    sp[8];
-    pf[4];
-    sp[4];
-    pf[4];
-    sp[4];
-    pf[6];
-    sp[50];
-  };
-
-  row.call {
-    pf[4];
-    sp[6];
-    pf[6];
-    sp[6];
-    pf[8];
-    sp[2];
-    pf[4];
-    sp[4];
-    pf[14];
-    sp[10];
-    pf[6];
-    sp[54];
-  };
-
-  row.call {
-    sp[60];
-    pf[6];
-    sp[58];
-  };
-
-  row.call {
-    sp[54];
-    pf[8];
-    sp[62];
-  };
-
-  row.call {
-    sp[4];
-    pf[14];
-    sp[30];
-    pf[4];
-    sp[64];
-    pf[4];
-    sp[4];
-  };
-
-  row.call {
-    sp[4];
-    pf[4];
-    sp[8];
-    pf[4];
-    sp[6];
-    pf[12];
-    sp[6];
-    pf[12];
-    sp[4];
-    pf[4];
-    sp[4];
-    pf[4];
-    sp[6];
-    pf[12];
-    sp[8];
-    pf[10];
-    sp[4];
-    pf[12];
-  };
-
-  row.call {
-    sp[4];
-    pf[14];
-    sp[6];
-    pf[4];
-    sp[8];
-    pf[4];
-    sp[8];
-    pf[4];
-    sp[8];
-    pf[10];
-    sp[6];
-    pf[4];
-    sp[8];
-    pf[4];
-    sp[4];
-    pf[4];
-    sp[4];
-    pf[4];
-    sp[8];
-    pf[4];
-    sp[4];
-  };
-
-  row.call {
-    sp[4];
-    pf[8];
-    sp[12];
-    pf[16];
-    sp[8];
-    pf[4];
-    sp[8];
-    pf[6];
-    sp[10];
-    pf[16];
-    sp[4];
-    pf[4];
-    sp[4];
-    pf[4];
-    sp[8];
-    pf[4];
-    sp[4];
-  };
-
-  row.call {
-    sp[4];
-    pf[4];
-    sp[4];
-    pf[4];
-    sp[8];
-    pf[4];
-    sp[20];
-    pf[4];
-    sp[8];
-    pf[4];
-    sp[12];
-    pf[4];
-    sp[16];
-    pf[4];
-    sp[4];
-    pf[6];
-    sp[6];
-    pf[4];
-    sp[4];
-  };
-
-  row.call {
-    sp[4];
-    pf[4];
-    sp[6];
-    pf[6];
-    sp[6];
-    pf[10];
-    sp[14];
-    pf[6];
-    sp[4];
-    pf[4];
-    sp[14];
-    pf[10];
-    sp[10];
-    pf[6];
-    sp[4];
-    pf[4];
-    sp[6];
-    pf[6];
-  };
-
-  row.call {
-    sp[124];
-  };
-
-  row.call {
-    sp[24];
-    pf[8];
-    sp[12];
-    pf[8];
-    sp[12];
-    pf[8];
-    sp[18];
-    pf[6];
-    sp[28];
-  };
-
-  row.call {
-    sp[20];
-    pf[6];
-    sp[4];
-    pf[6];
-    sp[4];
-    pf[6];
-    sp[4];
-    pf[6];
-    sp[4];
-    pf[6];
-    sp[4];
-    pf[6];
-    sp[12];
-    pf[8];
-    sp[28];
-  };
-
-  row.call {
-    sp[20];
-    pf[4];
-    sp[8];
-    pf[4];
-    sp[4];
-    pf[4];
-    sp[8];
-    pf[4];
-    sp[4];
-    pf[4];
-    sp[8];
-    pf[4];
-    sp[8];
-    pf[6];
-    sp[2];
-    pf[4];
-    sp[28];
-  };
-
-  row.call {
-    sp[28];
-    pf[6];
-    sp[6];
-    pf[4];
-    sp[8];
-    pf[4];
-    sp[12];
-    pf[6];
-    sp[8];
-    pf[4];
-    sp[6];
-    pf[4];
-    sp[28];
-  };
-
-  row.call {
-    sp[24];
-    pf[6];
-    sp[10];
-    pf[4];
-    sp[8];
-    pf[4];
-    sp[8];
-    pf[6];
-    sp[10];
-    pf[20];
-    sp[24];
-  };
-
-  row.call {
-    sp[20];
-    pf[4];
-    sp[16];
-    pf[4];
-    sp[8];
-    pf[4];
-    sp[4];
-    pf[4];
-    sp[28];
-    pf[4];
-    sp[28];
-  };
-
-  row.call {
-    sp[20];
-    pf[16];
-    sp[8];
-    pf[8];
-    sp[8];
-    pf[16];
-    sp[16];
-    pf[4];
-    sp[28];
-  };
-
-  row.call {
-    sp[124];
-  };
-
-  pf[140];
-  puts;
-  pf[140];
-
-  puts;
-  ### padding ###
-  ### padding ###
-  ######
-)*""
