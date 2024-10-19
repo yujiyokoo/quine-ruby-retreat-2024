3011ebb149d181f7a417b01905b62c72df9864fc
Yuji Yokoo <yujiyokoo@gmail.com>
2024-10-19 20:15:12 +1100

    Adjust display

--- ruby_retreat_2024.rb
+++ ruby_retreat_2024.rb
@@ -9,16 +9,14 @@ eval$s=%w(
 
   pf[140];
   puts;
-  pf[140];
-  puts;
 
   row.call {
     sp[124];
   };
 
   row.call {
-    pf[12];
-    sp[28];
+    pf[14];
+    sp[26];
     pf[4];
     sp[80];
   };
@@ -33,16 +31,16 @@ eval$s=%w(
   };
 
   row.call {
-    pf[12];
-    sp[8];
+    pf[14];
+    sp[6];
     pf[4];
     sp[8];
     pf[4];
     sp[4];
     pf[12];
-    sp[8];
+    sp[6];
     pf[4];
-    sp[8];
+    sp[10];
     pf[4];
     sp[48];
   };
@@ -55,8 +53,8 @@ eval$s=%w(
     pf[4];
     sp[4];
     pf[4];
-    sp[8];
-    pf[4];
+    sp[6];
+    pf[6];
     sp[4];
     pf[4];
     sp[8];
@@ -79,45 +77,41 @@ eval$s=%w(
     sp[4];
     pf[4];
     sp[4];
-    pf[4];
-    sp[52];
+    pf[6];
+    sp[50];
   };
 
   row.call {
     pf[4];
-    sp[8];
-    pf[4];
-    sp[8];
+    sp[6];
+    pf[6];
+    sp[6];
+    pf[8];
+    sp[2];
     pf[4];
     sp[4];
-    pf[4];
-    sp[4];
-    pf[12];
-    sp[12];
-    pf[4];
-    sp[56];
+    pf[14];
+    sp[10];
+    pf[6];
+    sp[54];
   };
 
   row.call {
     sp[60];
-    pf[4];
-    sp[60];
+    pf[6];
+    sp[58];
+  };
+
+  row.call {
+    sp[54];
+    pf[8];
+    sp[62];
   };
 
   row.call {
     sp[4];
-    pf[12];
-    sp[40];
-    pf[4];
-    sp[64];
-  };
-
-  row.call {
-    sp[4];
-    pf[4];
-    sp[8];
-    pf[4];
-    sp[28];
+    pf[14];
+    sp[30];
     pf[4];
     sp[64];
     pf[4];
@@ -129,34 +123,34 @@ eval$s=%w(
     pf[4];
     sp[8];
     pf[4];
-    sp[8];
-    pf[8];
-    sp[8];
+    sp[6];
+    pf[12];
+    sp[6];
     pf[12];
     sp[4];
     pf[4];
     sp[4];
     pf[4];
+    sp[6];
+    pf[12];
     sp[8];
-    pf[8];
-    sp[12];
-    pf[8];
+    pf[10];
     sp[4];
     pf[12];
   };
 
   row.call {
     sp[4];
-    pf[12];
-    sp[8];
+    pf[14];
+    sp[6];
     pf[4];
     sp[8];
     pf[4];
     sp[8];
     pf[4];
     sp[8];
-    pf[8];
-    sp[8];
+    pf[10];
+    sp[6];
     pf[4];
     sp[8];
     pf[4];
@@ -177,8 +171,8 @@ eval$s=%w(
     sp[8];
     pf[4];
     sp[8];
-    pf[4];
-    sp[12];
+    pf[6];
+    sp[10];
     pf[16];
     sp[4];
     pf[4];
@@ -205,8 +199,8 @@ eval$s=%w(
     sp[16];
     pf[4];
     sp[4];
-    pf[4];
-    sp[8];
+    pf[6];
+    sp[6];
     pf[4];
     sp[4];
   };
@@ -214,22 +208,22 @@ eval$s=%w(
   row.call {
     sp[4];
     pf[4];
-    sp[8];
-    pf[4];
-    sp[8];
-    pf[8];
-    sp[16];
-    pf[4];
+    sp[6];
+    pf[6];
+    sp[6];
+    pf[10];
+    sp[14];
+    pf[6];
     sp[4];
     pf[4];
-    sp[16];
-    pf[8];
-    sp[12];
-    pf[4];
+    sp[14];
+    pf[10];
+    sp[10];
+    pf[6];
     sp[4];
     pf[4];
-    sp[8];
-    pf[4];
+    sp[6];
+    pf[6];
   };
 
   row.call {
@@ -243,33 +237,31 @@ eval$s=%w(
     pf[8];
     sp[12];
     pf[8];
-    sp[20];
-    pf[4];
+    sp[18];
+    pf[6];
     sp[28];
   };
 
   row.call {
     sp[20];
-    pf[4];
-    sp[8];
-    pf[4];
+    pf[6];
     sp[4];
-    pf[4];
-    sp[8];
-    pf[4];
+    pf[6];
     sp[4];
-    pf[4];
-    sp[8];
-    pf[4];
+    pf[6];
+    sp[4];
+    pf[6];
+    sp[4];
+    pf[6];
+    sp[4];
+    pf[6];
     sp[12];
     pf[8];
     sp[28];
   };
 
   row.call {
-    sp[32];
-    pf[4];
-    sp[4];
+    sp[20];
     pf[4];
     sp[8];
     pf[4];
@@ -277,39 +269,43 @@ eval$s=%w(
     pf[4];
     sp[8];
     pf[4];
+    sp[4];
+    pf[4];
     sp[8];
     pf[4];
-    sp[4];
+    sp[8];
+    pf[6];
+    sp[2];
     pf[4];
     sp[28];
   };
 
   row.call {
     sp[28];
-    pf[4];
-    sp[8];
+    pf[6];
+    sp[6];
     pf[4];
     sp[8];
     pf[4];
     sp[12];
-    pf[4];
+    pf[6];
     sp[8];
     pf[4];
-    sp[8];
+    sp[6];
     pf[4];
     sp[28];
   };
 
   row.call {
     sp[24];
-    pf[4];
-    sp[12];
-    pf[4];
-    sp[8];
+    pf[6];
+    sp[10];
     pf[4];
     sp[8];
     pf[4];
-    sp[12];
+    sp[8];
+    pf[6];
+    sp[10];
     pf[20];
     sp[24];
   };
@@ -351,13 +347,5 @@ eval$s=%w(
   puts;
   ### padding ###
   ### padding ###
-  ### padding ###
-  ### padding ###
-  ### padding ###
-  ### padding ###
-  ### padding ###
-  ### padding ###
-  ### padding ###
-  ### padding ###
-  ###########
+  ######
 )*""
