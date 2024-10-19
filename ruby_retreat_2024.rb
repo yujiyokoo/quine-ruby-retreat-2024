365a15445f25f3a455f68ebf100c1a1b96070eb6
Yuji Yokoo <yujiyokoo@gmail.com>
2024-10-19 15:50:55 +1100

    Add WIP ruby retreat 2024

---
+++ ruby_retreat_2024.rb
@@ -0,0 +1,294 @@
+eval$s=%w(
+  s=%(eval$s=%w(#{$s})*"");
+  f = -> n { s.slice!(0, n) };
+
+  print(f[6]);
+  print(32.chr * 14);
+  print(f[2]);
+  puts;
+
+  2.times {
+    print(f[2]);
+    print(32.chr * 4);
+    print(f[2]);
+    print(32.chr * 12);
+    print(f[2]);
+    puts;
+  };
+
+  print(f[6]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 2);
+  print(f[6]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  puts;
+
+  print(f[4]);
+  print(32.chr * 6);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  puts;
+
+  print(f[2]);
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 2);
+  print(f[4]);
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 2);
+  print(f[2]);
+  puts;
+
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 2);
+  print(f[6]);
+  print(32.chr * 6);
+  print(f[2]);
+  puts;
+
+  print(32.chr * 30);
+  print(f[2]);
+  puts;
+
+  print(32.chr * 2);
+  print(f[6]);
+  print(32.chr * 20);
+  print(f[2]);
+  puts;
+
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 14);
+  print(f[2]);
+  print(32.chr * 32);
+  print(f[2]);
+  puts;
+
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[4]);
+  print(32.chr * 4);
+  print(f[6]);
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[6]);
+  print(32.chr * 4);
+  print(f[4]);
+  print(32.chr * 2);
+  print(f[6]);
+  puts;
+
+  print(32.chr * 2);
+  print(f[6]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[4]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  puts;
+
+  print(32.chr * 2);
+  print(f[4]);
+  print(32.chr * 6);
+  print(f[8]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 6);
+  print(f[8]);
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 2);
+  print(f[2]);
+
+  print(32.chr * 4);
+  print(f[2]);
+  puts;
+
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 10);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 6);
+  print(f[2]);
+  print(32.chr * 8);
+  print(f[2]);
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  puts;
+
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[4]);
+  print(32.chr * 8);
+  print(f[2]);
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 8);
+  print(f[4]);
+  print(32.chr * 6);
+  print(f[2]);
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  puts;
+
+  puts;
+
+  print(32.chr * 12);
+  print(f[4]);
+  print(32.chr * 6);
+  print(f[4]);
+  print(32.chr * 6);
+  print(f[4]);
+  print(32.chr * 10);
+  print(f[2]);
+  puts;
+
+  print(32.chr * 10);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 6);
+  print(f[4]);
+  puts;
+
+  print(32.chr * 16);
+  print(f[2]);
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 2);
+  print(f[2]);
+  puts;
+
+  print(32.chr * 14);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 6);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  puts;
+
+  print(32.chr * 12);
+  print(f[2]);
+  print(32.chr * 6);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 6);
+  print(f[10]);
+  puts;
+
+  print(32.chr * 10);
+  print(f[2]);
+  print(32.chr * 8);
+  print(f[2]);
+  print(32.chr * 4);
+  print(f[2]);
+  print(32.chr * 2);
+  print(f[2]);
+  print(32.chr * 14);
+  print(f[2]);
+  puts;
+
+  print(32.chr * 10);
+  print(f[8]);
+  print(32.chr * 4);
+  print(f[4]);
+  print(32.chr * 4);
+  print(f[8]);
+  print(32.chr * 8);
+  print(f[2]);
+
+  #22.times {|i|
+    puts(f[2] + 32.chr * 26 + f[2])
+  };
+  puts(s)
+  ##
+)*""
